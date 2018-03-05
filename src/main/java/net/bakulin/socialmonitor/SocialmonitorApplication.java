3c3958be6da2e7d675892f3de33ce8b50769a021
Alexey Bakulin <abakulin@griddynamics.com>
2018-03-05 09:38:19 +0400

    Init maven module on Spring

---
+++ src/main/java/net/bakulin/socialmonitor/SocialmonitorApplication.java
@@ -0,0 +1,12 @@
+package net.bakulin.socialmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMonitorApplication.class, args);
+	}
+}
